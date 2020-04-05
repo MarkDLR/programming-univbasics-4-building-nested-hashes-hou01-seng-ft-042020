54a9108a02271084e89997f8c74e9aabe19d1055
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-05 03:40:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
    monopoly_with_second_tier = (base_hash[:railroads])
-   monopoly_with_second_tier[:pieces]
+   monopoly_with_second_tier[:pieces] = 4
 end
 
 def monopoly_with_third_tier
